35d09adc59995f873f24c057f996997a5e82facb
Mayank Pandey <mayank.nice9411@gmail.com>
2024-11-16 16:20:12 +0530

    Fixed Sigin issue

--- lib/models/User.ts
+++ lib/models/User.ts
@@ -6,8 +6,8 @@ import { IUser } from '../interfaces/IUser';
 const userSchema = new Schema<IUser>({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-  phoneNumber: {type: String, required: true, unique: true, maxlength: 10},
+  password: { type: String },
+  phoneNumber: {type: String, unique: true, maxlength: 10},
   createdDate: { type: Date, default: Date.now },  // Automatically set to the current date
   lastLogin: { type: Date, default: null },         // Updated at each login
   loginCount: { type: Number, default: 0 },         // Incremented with each login
